0cd0872ff36e4ae5c584d0144bf94a627785f1b7
Ward Poelmans <wpoely86@gmail.com>
2021-02-23 10:55:51 +0100

    update to 2021

--- lib/vsc/administration/slurm/sync.py
+++ lib/vsc/administration/slurm/sync.py
@@ -1,5 +1,5 @@
 #
-# Copyright 2013-2020 Ghent University
+# Copyright 2013-2021 Ghent University
 #
 # This file is part of vsc-administration,
 # originally created by the HPC team of Ghent University (http://ugent.be/hpc/en),
