7ecf6414f6db91616f6e2c5415ed060707c921b9
Ward Poelmans <wpoely86@gmail.com>
2021-02-24 09:38:52 +0100

    version bump

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if sys.version_info < (3, 0):
     install_requires.append('enum34')
 
 PACKAGE = {
-    'version': '2.4.4',
+    'version': '2.4.5',
     'author': [ag, jt],
     'maintainer': [ag, jt],
     'tests_require': ['mock'],
