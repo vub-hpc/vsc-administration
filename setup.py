38c39da0e195f88eac2fe8a10ffaef52bb0bed60
Andy Georges <itkovian@gmail.com>
2021-05-28 16:24:53 +0200

    bump: version to 2.5.1

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ if sys.version_info < (3, 0):
     install_requires.append('enum34')
 
 PACKAGE = {
-    'version': '2.5.0',
+    'version': '2.5.1',
     'author': [ag, jt],
     'maintainer': [ag, jt],
     'tests_require': ['mock'],
